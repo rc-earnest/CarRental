833f4f1f52258e5d5deeaee6ea45af27b67bb996
rc-earnest <rc.earnest04@gmail.com>
2025-04-14 08:49:56 -0600

    Added Header

--- RentalForm.vb
+++ RentalForm.vb
@@ -1,4 +1,9 @@
-﻿Option Explicit On
+﻿'Rudy Earnest
+'Car Rental Form
+'RCET 2265
+'Spring 2025
+'https://github.com/rc-earnest/CarRental.git
+Option Explicit On
 Option Strict On
 Option Compare Binary
 Public Class RentalForm
